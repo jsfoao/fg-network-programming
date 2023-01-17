81ca51b89e6355cd5b41229db32026b7973f5fac
lokr934 <emilia.lokrantz@futuregames.nu>
2023-01-17 14:22:49 +0100

    Spawner
    Added a spawner prefab that can spawn a ball prefab on input.

---
+++ Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,16 @@
+using Alteruna;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour
+{
+    [SerializeField] private Spawner spawner;
+    [SerializeField] private Vector3 spawnPos;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            spawner.Spawn(0, spawnPos);
+        }
+    }
+}
