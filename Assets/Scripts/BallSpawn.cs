686aa57301ef0aceab7c30a1af80c793de8e101d
harofax <daniel.kh555@gmail.com>
2023-01-24 14:47:24 +0100

    spawn ball method

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -5,8 +5,11 @@ public class BallSpawn : MonoBehaviour
 {
     [SerializeField] private Spawner spawner;
     [SerializeField] private Vector3 spawnPos;
-    
-    
+
+    public void SpawnBall()
+    {
+        spawner.Spawn(0, spawnPos);
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
