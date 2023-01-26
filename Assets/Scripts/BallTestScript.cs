96542ce4d23d43e4bec6b36505ca1dfd056c2a06
MartinReinBR <martin.blomquist@hotmail.com>
2023-01-26 14:05:32 +0100

    Dont worry about it

--- Assets/Scripts/BallTestScript.cs
+++ Assets/Scripts/BallTestScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BallTestScript : MonoBehaviour
 {
+    public float speed;
+    public float maxSpeed;
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LaunchBall();
     }
 
-    // Update is called once per frame
-    void Update()
+    //update is called once per frame
+    void update()
     {
-        
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+
+    }
+
+    private void LaunchBall()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        Vector2 direction = new Vector2(x, y);
+        rb.AddForce(direction * this.speed);
     }
 }
